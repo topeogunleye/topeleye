84ea05946c353d257eb5f738ac9d01727aa0413c
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-06 13:35:58 +0100

    Removes BrittanyChiang

--- src/templates/post.js
+++ src/templates/post.js
@@ -42,8 +42,8 @@ const PostTemplate = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>{title} | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/pensieve" />
+        <title>{title} | Tope Leye</title>
+        <link rel="canonical" href="https://topeleye.netlify.app/pensieve" />
       </Helmet>
 
       <StyledPostContainer>
